6825b6f71d43b870f59dac0849a521cb16de84a2
elf-mouse <ifmouz@gmail.com>
2023-03-17 11:44:58 +0800

    chore: update

--- templates/app/scripts/main.js
+++ templates/app/scripts/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from '@/views/layouts/app';
 import router from '@/routes';
 import BalmUI from 'balm-ui';
-// import BalmUIPlus from 'balm-ui-plus';
+import BalmUIPlus from 'balm-ui-plus';
 import $http from '@/plugins/http';
 import { balmUIConfig, balmUIPlusConfig } from '@/config/components';
 
@@ -11,7 +11,7 @@ function createMyApp() {
 
   app.use(router);
   app.use(BalmUI, balmUIConfig);
-  // app.use(BalmUIPlus, balmUIPlusConfig);
+  app.use(BalmUIPlus, balmUIPlusConfig);
   app.use($http);
 
   app.mount('#app');
