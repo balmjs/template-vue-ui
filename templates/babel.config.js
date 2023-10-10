9a887906c378eb32dcd4dee4f19aeb493d095d53
elf-mouse <ifmouz@gmail.com>
2023-10-10 17:33:11 +0800

    chore: update

--- templates/babel.config.js
+++ templates/babel.config.js
@@ -3,14 +3,17 @@ module.exports = function (api) {
     ? {
         modules: false,
         useBuiltIns: 'usage',
-        corejs: { version: '3.29', proposals: true }
+        corejs: { version: '3.30', proposals: true }
       }
     : {};
   const runtimeOptions = api.env('production') ? { corejs: 3 } : {};
 
   return {
     presets: [['@babel/preset-env', envOptions]],
-    plugins: [['@babel/plugin-transform-runtime', runtimeOptions]],
+    plugins: [
+      '@babel/plugin-proposal-optional-chaining',
+      ['@babel/plugin-transform-runtime', runtimeOptions]
+    ],
     env: {
       test: {
         presets: ['@babel/preset-env']
